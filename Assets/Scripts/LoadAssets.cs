ff9ebd3123051f68ec0b15db24043f052d2ad95a
Kevin Leung <kevinleung56@gmail.com>
2021-08-26 11:48:59 +1000

    70 percent

--- Assets/Scripts/LoadAssets.cs
+++ Assets/Scripts/LoadAssets.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 public class LoadAssets : MonoBehaviour
 {
     public GameObject redObj;
+
+    [SerializeField]
+    private GameObject blueObj;
     // Start is called before the first frame update
     void Start()
     {
-        
+        var redPosition = new Vector3(2, 0, 0);
+        var redRotation = new Quaternion(0, 0, 0, 0);
+        var bluePosition = new Vector3(-2, 0, 0);
+        var blueRotation = new Quaternion(0, 0, 0, 0);
+        var redObject = Instantiate(redObj, redPosition, redRotation);
+        var blueObject = Instantiate(blueObj, bluePosition, blueRotation);
     }
 
     // Update is called once per frame
