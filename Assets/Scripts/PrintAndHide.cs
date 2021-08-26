eec8058037f7ea8d4fb97bca9b6f1e13a91734c9
Kevin Leung <kevinleung56@gmail.com>
2021-08-26 12:07:05 +1000

    90 Percent

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ConsolePrint : MonoBehaviour
+public class PrintAndHide : MonoBehaviour
 {
     public string output;
     public string gameObject_name;
     public int i;
+    public Renderer rend;
     // Start is called before the first frame update
     void Start()
     {
