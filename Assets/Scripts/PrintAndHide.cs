4914819268cf579754c1c8d8ccc0ea75340d9564
Kevin Leung <kevinleung56@gmail.com>
2021-08-26 12:19:06 +1000

    100 Percent

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -7,17 +7,28 @@ public class PrintAndHide : MonoBehaviour
     public string output;
     public string gameObject_name;
     public int i;
+    public int random;
     public Renderer rend;
     // Start is called before the first frame update
     void Start()
     {
         i = 3;
+        random = Random.Range(200, 250);
         gameObject_name = this.gameObject.name;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (this.gameObject.tag == "Red" && i == 100)
+        {
+            this.gameObject.SetActive(false);
+        }
+        else if (this.gameObject.tag == "Blue" && i == random)
+        {
+            rend.enabled = false;
+        }
+
         i++;
         Debug.Log(gameObject_name + ":" + i.ToString());
     }
